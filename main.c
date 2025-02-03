52fe247b5699b2949c2acbb2b96961c8a746d637
Gavin McClure-Coleman <gmcclurecoleman@gmail.com>
2025-02-02 17:18:55 -0800

    added logic for the hexToBinary function to save the binary string output to a variable rather than just printing the string, this way it can be used later in the binary to base64 function

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 // Gavin McClure-Coleman
 // Program to convert Hexadecimal to Base64
-#include <stdio.h>   
+#include <stdio.h>  
+#include <stdlib.h>
+#include <string.h>
 
-void hexToBinary(char* hexString){
+
+char* hexToBinary(char* hexString){
     size_t i = 0;
+    int n = 500;
+    char tempString[5]; //size 5 to account for null char
+    char *binaryString = (char*) malloc(sizeof(char) * n);
+    // Initialize the binaryString to an empty string (so it doesnt print weird symbols)
+    binaryString[0] = '\0';
     // check if the string begins with 0X
     if(hexString[1] == 'x' || hexString[1] == 'X'){
         // set the index to two to begin at the actual start of the hex value
@@ -14,71 +22,80 @@ void hexToBinary(char* hexString){
     while(hexString[i]){
         switch(hexString[i]){
             case '0':
-                printf("0000");
+                strcat(binaryString, "0000");
                 break;
             case '1':
-                printf("0001");
+                strcat(binaryString, "0001");
                 break;
             case '2':
-                printf("0010");
+                strcat(binaryString, "0010");
                 break;
             case '3':
-                printf("0011");
+                strcat(binaryString, "0011");
                 break;
             case '4':
-                printf("0100");
+                strcat(binaryString, "0100");
                 break;
             case '5':
-                printf("0101");
+                strcat(binaryString, "0101");
                 break;
             case '6':
-                printf("0110");
+                strcat(binaryString, "0110");
                 break;
             case '7':
-                printf("0111");
+                strcat(binaryString, "0111");
                 break;
             case '8':
-                printf("1000");
+                strcat(binaryString, "1000");
                 break;
             case '9':
-                printf("1001");
+                strcat(binaryString, "1001");
                 break;
             case 'A':
             case 'a':
-                printf("1010");
+                strcat(binaryString, "1010");
                 break;
             case 'B':
             case 'b':
-                printf("1011");
+                strcat(binaryString, "1011");
                 break;
             case 'C':
             case 'c':
-                printf("1100");
+                strcat(binaryString, "1100");
                 break;
             case 'D':
             case 'd':
-                printf("1101");
+                strcat(binaryString, "1101");
                 break;
             case 'E':
             case 'e':
-                printf("1110");
+                strcat(binaryString, "1110");
                 break;
             case 'F':
             case 'f':
-                printf("1111");
+                strcat(binaryString, "1111");
                 break;
             default:
                 printf("\nHexadecimal character is invalid %c", hexString[i]);
         }
         i++;
     }
+    return binaryString;
+    free(binaryString);
 }
 
-void main()   
+int main(int argc, char *argv[])  
 {  
-    char hexString[100] = "0x1AC";
+    char *binaryString;
+    char hexString[100] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-    printf("\nBinary string: ");
+    binaryString = hexToBinary(hexString);
+    if (binaryString == NULL){
+        return -1;
+    }
 
-    hexToBinary(hexString);
+    printf("\nBinary string: %s", binaryString);
+
+    free(binaryString);
+    return 0;
 }
